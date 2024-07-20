2b46eb8183a5860fb655abb3fef81f1781bf7a84
area6080 <area6080@gmail.com>
2024-07-20 17:36:43 +0900

    Cpt.05

--- lib/next_page.dart
+++ lib/next_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class NextPage extends StatelessWidget {
+  const NextPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -8,7 +10,15 @@ class NextPage extends StatelessWidget {
         title: Text('KBOYのFlutter大学'),
       ),
       body: Container(
-        color: Colors.pink,
+        color: Colors.pink.shade100,
+        child: Center(
+          child: ElevatedButton(
+            child: Text("前の画面に戻る"),
+            onPressed: () {
+            Navigator.pop(context);
+            },
+          ),
+        ),
       ),
     );
   }
--- lib/test2.dart
+++ lib/test2.dart
@@ -13,56 +13,36 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.greenAccent.shade100),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key}); // const コンストラクタを追加
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text('KBOYのFlutter幼稚園２'),
+        title: Text('KBOYのFlutter幼稚園'),
       ),
       body: Center(
         child: ElevatedButton(
-          child: Text('次へ'),
+          child: Text("次の画面へ"),
           onPressed: () {
             Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => NextPage()),
+                context,
+                MaterialPageRoute(builder: (context) => const NextPage()),
+                // ここにボタンを押した時に呼ばれるコードを書く
             );
           },
-          // ここにボタンを押した時に呼ばれるコードを書く
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
