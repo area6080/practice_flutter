fbafaad7c1ce5180cf5d7cfa1340e08198eb41ed
area6080 <area6080@gmail.com>
2024-07-31 17:23:19 +0900

    Cpt.10

--- lib/third_page.dart
+++ lib/third_page.dart
@@ -10,8 +10,25 @@ class ThirdPage extends StatelessWidget {
         title: const Text('KBOYのFlutter王国'),
         centerTitle: true,
       ),
-    body: Container(
-      color: Colors.lightBlue.shade50,
+      body: Container(
+        width: double.infinity,
+        color: Colors.lightBlue.shade50,
+        child: ListView(
+          children: const <Widget>[
+            ListTile(
+              leading: Icon(Icons.map),
+              title: Text('Map'),
+            ),
+            ListTile(
+              leading: Icon(Icons.photo_album),
+              title: Text('Album'),
+            ),
+            ListTile(
+              leading: Icon(Icons.phone),
+              title: Text('Phone'),
+            ),
+          ],
+        ),
       ),
     );
   }
